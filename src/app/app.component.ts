804a6af1483e698b344a6cfb770a82510c5d9d05
Henri Okayama <henri.okayama@gmail.com>
2026-01-20 20:55:32 -0300

    passando valores para o template do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MeuBotaoComponent } from './meu-botao/meu-botao.component';
 import { BotaoFlatComponent } from './botao-flat/botao-flat.component';
-
+import { EventBindingComponent } from './event-binding/event-binding.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MeuBotaoComponent, BotaoFlatComponent],
+  imports: [RouterOutlet, MeuBotaoComponent, BotaoFlatComponent, EventBindingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  imports: [],
+  templateUrl: './event-binding.component.html',
+  styleUrl: './event-binding.component.css'
+})
+export class EventBindingComponent {
+  onButtonClick(){
+    console.log('onButtonClick')
+  }
+
+  onInput(event: Event){
+    console.log('onInput')
+    console.log('event', event)
+
+    const target = (event.target as HTMLInputElement).value;
+
+    console.log('value: ', target);
+  }
+
+  onBlur(){
+    console.log('onBlur')
+  }
+
+  onFocus({
+    console.log('onFocus');
+  }
+}
